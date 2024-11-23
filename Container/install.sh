2a2e10947e863ac6c160bff63062307863286095
David Rosenfeld <david.rosenfeld@gmail.com>
2024-11-23 08:28:52 +0000

    #8 - Added script and dockerfile for building container.

---
+++ Container/install.sh
@@ -0,0 +1,23 @@
+apt-get update
+apt-get install -y build-essential zlib1g-dev libffi-dev libssl-dev libsqlite3-dev libreadline-dev libbz2-dev libncurses5-dev libgdbm-dev liblzma-dev tk-dev wget git
+cd /tmp
+wget https://www.python.org/ftp/python/3.7.16/Python-3.7.16.tgz
+tar -xvzf Python-3.7.16.tgz
+cd /tmp/Python-3.7.16
+./configure --enable-optimizations
+make -j$(nproc)
+make altinstall
+which python3.7
+which pip3.7
+python3.7 -V
+cd /tmp
+rm -rf Python-3.7.16
+rm -f Python-3.7.16.tgz
+strip --strip-unneeded /usr/local/bin/python3.7
+strip --strip-unneeded /usr/local/lib/libpython3.7m.a
+apt-get remove --purge -y build-essential zlib1g-dev libffi-dev libssl-dev     libsqlite3-dev libreadline-dev libbz2-dev libncurses5-dev libgdbm-dev liblzma-dev tk-dev
+apt-get autoremove -y
+apt-get clean
+python3.7 -m ensurepip --upgrade
+python3.7 -m pip install --upgrade pip setuptools wheel
+cd /
